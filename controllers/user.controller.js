48875bbccc20eb2500292b03eba916b03e18fdd0
KOSSY <abonekosiso@gmail.com>
2022-04-01 23:42:43 +0100

    adding routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,16 @@
 const User = require('../models/user')
 const mongoose = require('mongoose')
 
-export default('/', (req,res) => {
+exports.default('/', (req,res) => {
     res.status(200).json({
         "message": "route is working",
         "status": "ok"
     })
 })
 
-export.getAllUsers('/', async(req, res) => {
+exports.getAllUsers('/', async(req, res) => {
     try{
-        const allUsers = await mongoose.User.find();
+        const allUsers = await User.find();
         res.status(200).send(allUsers)
     }
     catch{
