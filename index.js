aa1ef073b73d98012017c223ccc8a4b86d039238
KOSSY <abonekosiso@gmail.com>
2022-04-13 16:12:08 +0100

    made an async mongoose connection in the database.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 require('dotenv').config()
+require('./database').connect()
 const express = require('express');
 const app = express()
-const mongoose = require('mongoose');
 const bookRouter = require('./routes/book.route')
 const registerRouter = require('./routes/register.route')
 
-mongoose.connect(
-    process.env.URL
-  )
-  .then(()=>console.log('connected'))
-  .catch(e=>console.log(e));
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
 
 app.use('/register', registerRouter);
 app.use('/book', bookRouter);
