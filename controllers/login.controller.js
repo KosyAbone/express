6e4f6b9dcf22ce5888b72a68c08a9af2f1a351be
KOSSY <abonekosiso@gmail.com>
2022-04-15 23:54:53 +0100

    working on login controller

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -3,5 +3,9 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 exports.login = async(req, res) => {
-    
+    const { username, password } = req.body
+
+    if(username && password && await bcrypt.compare(User.password, password)){
+        
+    }
 }
