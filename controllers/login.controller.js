3961d557faa73b909268d6e12785c2eb3e2167c1
KOSSY <abonekosiso@gmail.com>
2022-04-16 01:05:51 +0100

    login controller working well now with jwt integration

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,10 +1,35 @@
+require('dotenv').config()
 const User = require('../models/register.model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const secretKey = process.env.SECRET_KEY
 
 exports.login = async(req, res) => {
-    const { username, password } = req.body
+    const { email, password } = req.body
 
-    
-    
+    if(!(email && password)) 
+        return res.status(400).send("Both inputs are needed.");
+
+
+    try{
+        const user = await User.findOne({email})
+
+        if(!user)
+            return res.status(404).send("Incorrect Login Details.")
+
+        if(user && await bcrypt.compare(password, user.password)){
+            const token = jwt.sign(
+                {id: user._id, email}, 
+                secretKey, 
+                {expiresIn: '2h'})
+        
+            user.token = token;
+
+            res.status(200).json(user)
+        }
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: err.message})
+    }
 }
--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const app = express()
 const bookRouter = require('./routes/book.route')
 const registerRouter = require('./routes/register.route')
+const loginRouter = require('./routes/login.route')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/register', registerRouter);
 app.use('/book', bookRouter);
+app.use('/login', loginRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Express Server listening on port ${process.env.PORT}`);
