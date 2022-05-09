c88ef1434902a34101a73a6251bf26af68dc2e60
KOSSY <abonekosiso@gmail.com>
2022-05-09 23:45:24 +0100

    cleanup import routes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-require('./database').connect()
+require('./Backend/database').connect()
 const express = require('express');
 const app = express()
-const bookRouter = require('../Backend/routes/book.route')
-const registerRouter = require('../Backend/routes/register.route')
-const loginRouter = require('../Backend/routes/login.route')
+const bookRouter = require('./Backend/routes/book.route')
+const registerRouter = require('./Backend/routes/register.route')
+const loginRouter = require('./Backend/routes/login.route')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
