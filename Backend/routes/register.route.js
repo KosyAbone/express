81c834710ab8d33293619384a6bf4b8f5e3e964e
KOSSY <abonekosiso@gmail.com>
2022-08-29 23:54:35 +0100

    rev update

--- Backend/routes/register.route.js
+++ Backend/routes/register.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const registerController = require('../../Backend/controllers/register.controller')
 
-// router.get('/', registerController.test);
-// router.post('/', registerController.createUser);
+router.get('/', registerController.test);
+router.post('/', registerController.createUser);
 
 module.exports = router;
