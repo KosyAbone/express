6369ecb523a8fa0a2a0a9cce270bd9998a57b83e
Kosiso Abone <abonekosiso@gmail.com>
2023-10-03 23:15:27 -0400

    update

--- Backend/controllers/book.controller.js
+++ Backend/controllers/book.controller.js
@@ -59,14 +59,14 @@ exports.updateBook = async(req, res) => {
     
 }
 
-// exports.deleteBook = async(req, res) => {
-//     try{
-//         const book = await Book.findOneAndDelete({name: req.params.name})
-//         if(!book){
-//             return res.status(404).send('Book Does Not Exist!')
-//         }
-//         res.status(200).json({message: "delete successful"})
-//     }catch(err){
-//         res.status(500).json({message: err.message})
-//     }
-// }
+exports.deleteBook = async(req, res) => {
+    try{
+        const book = await Book.findOneAndDelete({name: req.params.name})
+        if(!book){
+            return res.status(404).send('Book Does Not Exist!')
+        }
+        res.status(200).json({message: "delete successful"})
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
